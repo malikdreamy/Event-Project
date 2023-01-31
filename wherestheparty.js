ac09e556fb4a942035458484c65eb48626f1ca2e
nsolo95 <n.cape44@gmail.com>
2023-01-31 18:38:23 -0500

    Update: replace cards with material card
    Refactor: break up code into functions
    
    Update: add more comments to js

--- wherestheparty.js
+++ wherestheparty.js
@@ -1,135 +1,116 @@
+/**
+ *
+ * @param {*} data
+ * @returns
+ * @description
+ * @algorithm
+ *
+ */ // takes data and creates html  card with image, price, and date. and returns string.
+const makeCardHtml = (data) => {
+ 
+  return `
+  <div class="card sticky-action left-align grey darekn-2">
+  <div class="card-image">
+    <img src=${data.images[1].url}>
+  </div>
+  <div class="card-stacked">
+    <div class="card-content">
+      <p style="color:black">${data._embedded.venues[0].address.line1}.</p>
+      <div class=" row" style="padding-top: 25px">
+      <a href="#" class="col m6 no-padding" style="color:black">$${data.priceRanges[0].min}-$${data.priceRanges[0].max}</a>
+      <a href="#" class="col m6 no-padding" style="color:white">${data.dates.start.localDate}</a>
+      </div>
+    </div>
+  </div>
+</div>`;
+};
 
+/**
+ *
+ * @param {*} event
+ * @returns {boolean}
+ * @description
+ */ // if event has price ranges it will return true. otherwise if event has no price range boolean returns false.
+const hasPriceRangesField = (event) => {
+  return Boolean(event?.priceRanges?.length);
+};
+
+/**
+ *
+ * @param {*} selectedGenre
+ * @param {*} city
+ * @returns void
+ * @description
+ * @algorithm
+ * start
+ * get inputs genre,city
+ * fetch events using inputes
+ * then get event lists
+ * then remove events without price
+ * then covert each event to card html
+ * then join all card html to one string
+ * add the string to the result element on the page
+ * end
+ */ // if there is no genre or city selected, return nothing.
+const fetchEventsAndDisplay = (selectedGenre = document.querySelector('input[name="genre"]:checked').value, city = document.querySelector("#city")) => {
+  // early return
+  if (!selectedGenre || !city?.value) {
+    return;
+  }
+
+  let ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=${selectedGenre}&preferredCountry=us&genreName=${selectedGenre}k&preferredCountry=us&city=${city.value}&size=60`;
+
+  //fetch events using inputes
+  fetch(ticketMasterUrl)
+    .then((data) => data.json())
+    .then((completedata) => {
+      console.log(completedata);
+
+      // then get event lists
+      const /** @type {Array} */ dataEventsArray = completedata._embedded.events; //creates an Array out of data.
+
+      // then join all card html to one string
+      //f(x)->y === makeCardHtml(eventInformation)-> eventCardHtmlString
+      // [e1,e2,e3] => makeCardHtml() ==> [e1html,e2html,e3html] => e1html contents + ... + eNhtml contents
+      const cardsHtml = dataEventsArray
+        .filter(hasPriceRangesField) //remove items without price rances
+        .map((data) => makeCardHtml(data)) //convert events to htmlcard
+        .join(""); //join events to one big string
+
+      //add all the cards to the page
+      document.getElementById("results").innerHTML = cardsHtml;
+    })
+    .catch((err) => {
+      console.log(err);
+      document.getElementById("results").innerHTML = "Error: failed to fetch events";
+    });
+};
+
+const getCurrentCity = (url = `https://get.geojs.io/v1/ip/geo.json`) => {
+  return fetch(url).then((data) => {
+    return data.json();
+  });
+};
+
+/**
+ *
+ */
 const loadPage = () => {
   // FETCH FOR USER IP ADDRESS AND GEOLOCATION
-  let url = `https://get.geojs.io/v1/ip/geo.json`
-  fetch(url).then((data) => {
-    //console.log(data);
-    return data.json();
-  }).then((completedata) => {
-    let txt = "";
-    console.log(completedata.city);
-    cityName = completedata.city;
-    
-    txt = ``
-document.getElementById("googlemap").innerHTML = txt;
-let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=music&size=39&city=${cityName}`
-  
-  fetch(localEventUrl).then((data) => {
-    //console.log(data);
-    return data.json();
-  }).then((completedata) => {
-    console.log(completedata);
-    let txt = "";
-    //let br = document.createElement("br")
-    let dataEvents = completedata._embedded.events
-    dataEvents.forEach((data) => {
-     if (data.hasOwnProperty("priceRanges")){
-      txt += ` <div class="concert-card">
-        <h5 class="title">${data.name}</h5>
-        <img src=${data.images[1].url} alt="img" class="trendingImages">
-        <div class="concert-info">
-        <p class="concert-date">${"Date: " + data.dates.start.localDate}</p>
-        <p class="concert-address">${"Address :" + data._embedded.venues[0].address.line1}</p>
-        <p class="price">${"Price Ranges: " + "$" + data.priceRanges[0].min + " - " + "$" + data.priceRanges[0].max}</p>
-        </div>
-        </div>`;
-     }
-      //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ; 
-      let concertCard = document.querySelectorAll(".concert-card");
-
-      // concertCard.forEach((card) =>{
-      //   if(card.querySelector(".price").innerHTML.trim().length === 0){
-      //     card.style.display = "none";
-      //   } else {
-          
-      //   }
-
-      // })
-      
-
-      document.getElementById("results").innerHTML = txt;
-    });
-  }).catch((err) => {
-    console.log(err);
+  getCurrentCity().then((responsJson) => {
+    document.getElementById("city").value = responsJson.city; // tells you what city you are in.
+    fetchEventsAndDisplay("music", { value: responsJson.city }); 
   });
-
-
-  });
-
-  // FETCH FOR TRENDING CONCERTS IN AREA
-
-  // let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=music&size=2&city=${cityName}`
-  
-  // fetch(localEventUrl).then((data) => {
-  //   //console.log(data);
-  //   return data.json();
-  // }).then((completedata) => {
-  //   console.log(completedata);
-  //   let txt = "";
-  //   //let br = document.createElement("br")
-  //   let dataEvents = completedata._embedded.events
-  //   dataEvents.forEach((data) => {
-  //     txt += ` <div class="card">
-  //       <h1 class="title">${data.name}</h1>
-  //       <img src=${data.images[1].url} alt="img" class="images">
-  //       <p>${data.dates.start.dateTime}</p>
-  //       <p class="category">${data._embedded.venues[0].address.line1}</p>
-  //       <p class="price"></p>
-  //       </div>`;
-  //     //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ; 
-  //     document.getElementById("results").innerHTML = txt;
-  //   });
-  // }).catch((err) => {
-  //   console.log(err);
-  // });
-
 
   // FETCH FOR USER SEARCH
-document.querySelector("#trending").style.display = "none";
-  let button = document.getElementById('submitBtn');
-  let radioButtons = document.getElementsByName('input');
-  button.addEventListener("click", () => {
 
-    let userMessage = "";
-    let city = document.querySelector("#city");
-    if (city.value == "") {
-      userMessage = ` Please Enter Your City!`
-      document.getElementById("userSelection").append(userMessage)
-    } else {
-      let selectedGenre = document.querySelector('input[name="genre"]:checked').value;
-      let ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=${selectedGenre}&preferredCountry=us&genreName=${selectedGenre}k&preferredCountry=us&city=${city.value}&size=60`
-      fetch(ticketMasterUrl).then((data) => {
-        //console.log(data);
-        return data.json();
-      }).then((completedata) => {
-        console.log(completedata);
-        let txt = "";
-        //let br = document.createElement("br")
-        let dataEvents = completedata._embedded.events
-        dataEvents.forEach((data) => {
-          if (data.hasOwnProperty("priceRanges")){
-          txt += ` <div class="concert-card">
-          <h5 class="title">${data.name}</h5>
-          <img src=${data.images[1].url} alt="img" class="trendingImages">
-          <div class="concert-info">
-          <p class="concert-date">${data.dates.start.localDate}</p>
-          <p class="concert-address">${data._embedded.venues[0].address.line1}</p>
-          <p class="price">${"Price Ranges: " + "$" + data.priceRanges[0].min + " - " + "$" + data.priceRanges[0].max}</p>
-          </div>
-          </div>`;
-          }
-          //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ; 
-          document.getElementById("results").innerHTML = txt;
+  document.querySelector("#trending").style.display = "none";
 
-        });
-      }).catch((err) => {
-        console.log(err);
-      });
-
-    }
+  let userSelectionForm = document.getElementById("userSelection");
+  userSelectionForm.addEventListener("submit", (event) => {
+    event.preventDefault(); //prevents page from reloading
+    fetchEventsAndDisplay(); // loads cards to results page 
   });
+};
 
-}
-window.addEventListener("load", loadPage)
-
-
+window.addEventListener("load", loadPage);
