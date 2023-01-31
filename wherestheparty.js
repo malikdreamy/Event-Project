fd85597d3d38203a97a3748177a220699a09d207
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-01-30 20:29:13 -0500

    Update wherestheparty.js

--- wherestheparty.js
+++ wherestheparty.js
@@ -1,33 +1,5 @@
 const loadPage = () => {
   // FETCH FOR USER IP ADDRESS AND GEOLOCATION
-  let url = `https://get.geojs.io/v1/ip/geo.json`;
-  fetch(url)
-    .then((data) => {
-      //console.log(data);
-      return data.json();
-    })
-    .then((completedata) => {
-      let txt = "";
-      console.log(completedata.city);
-      cityName = completedata.city;
-
-      txt = ``;
-      document.getElementById("googlemap").innerHTML = txt;
-      let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=music&size=39&city=${cityName}`;
-
-      fetch(localEventUrl)
-        .then((data) => {
-          //console.log(data);
-          return data.json();
-        })
-        .then((completedata) => {
-          console.log(completedata);
-          let txt = "";
-          //let br = document.createElement("br")
-          let dataEvents = completedata._embedded.events;
-          dataEvents.forEach((data) => {
-            if (data.hasOwnProperty("priceRanges")) {
-              txt += ` <div class="concert-card">
   let url = `https://get.geojs.io/v1/ip/geo.json`
   fetch(url).then((data) => {
     //console.log(data);
@@ -36,11 +8,11 @@ const loadPage = () => {
     let txt = "";
     console.log(completedata.city);
     cityName = completedata.city;
-
+    
     txt = ``
 document.getElementById("googlemap").innerHTML = txt;
 let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=music&size=39&city=${cityName}`
-
+  
   fetch(localEventUrl).then((data) => {
     //console.log(data);
     return data.json();
@@ -55,40 +27,25 @@ let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1co
         <h5 class="title">${data.name}</h5>
         <img src=${data.images[1].url} alt="img" class="trendingImages">
         <div class="concert-info">
-@@ -36,31 +33,33 @@ const loadPage = () => {
+        <p class="concert-date">${"Date: " + data.dates.start.localDate}</p>
+        <p class="concert-address">${"Address :" + data._embedded.venues[0].address.line1}</p>
         <p class="price">${"Price Ranges: " + "$" + data.priceRanges[0].min + " - " + "$" + data.priceRanges[0].max}</p>
         </div>
         </div>`;
-            }
-            //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ;
-            let concertCard = document.querySelectorAll(".concert-card");
-
-            // concertCard.forEach((card) =>{
-            //   if(card.querySelector(".price").innerHTML.trim().length === 0){
-            //     card.style.display = "none";
-            //   } else {
      }
       //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ; 
       let concertCard = document.querySelectorAll(".concert-card");
 
-            //   }
       // concertCard.forEach((card) =>{
       //   if(card.querySelector(".price").innerHTML.trim().length === 0){
       //     card.style.display = "none";
       //   } else {
-
+          
       //   }
 
-            // })
       // })
+      
 
-
-            document.getElementById("results").innerHTML = txt;
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
       document.getElementById("results").innerHTML = txt;
     });
   }).catch((err) => {
@@ -101,16 +58,23 @@ let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1co
   // FETCH FOR TRENDING CONCERTS IN AREA
 
   // let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=music&size=2&city=${cityName}`
-
   
   // fetch(localEventUrl).then((data) => {
   //   //console.log(data);
   //   return data.json();
-@@ -77,56 +76,60 @@ const loadPage = () => {
+  // }).then((completedata) => {
+  //   console.log(completedata);
+  //   let txt = "";
+  //   //let br = document.createElement("br")
+  //   let dataEvents = completedata._embedded.events
+  //   dataEvents.forEach((data) => {
+  //     txt += ` <div class="card">
+  //       <h1 class="title">${data.name}</h1>
+  //       <img src=${data.images[1].url} alt="img" class="images">
+  //       <p>${data.dates.start.dateTime}</p>
   //       <p class="category">${data._embedded.venues[0].address.line1}</p>
   //       <p class="price"></p>
   //       </div>`;
-  //     //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ;
   //     //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ; 
   //     document.getElementById("results").innerHTML = txt;
   //   });
@@ -120,9 +84,6 @@ let localEventUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1co
 
 
   // FETCH FOR USER SEARCH
-  document.querySelector("#trending").style.display = "none";
-  let button = document.getElementById("submitBtn");
-  let radioButtons = document.getElementsByName("input");
 document.querySelector("#trending").style.display = "none";
   let button = document.getElementById('submitBtn');
   let radioButtons = document.getElementsByName('input');
@@ -131,27 +92,10 @@ document.querySelector("#trending").style.display = "none";
     let userMessage = "";
     let city = document.querySelector("#city");
     if (city.value == "") {
-      userMessage = ` Please Enter Your City!`;
-      document.getElementById("userSelection").append(userMessage);
       userMessage = ` Please Enter Your City!`
       document.getElementById("userSelection").append(userMessage)
     } else {
       let selectedGenre = document.querySelector('input[name="genre"]:checked').value;
-      let ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=${selectedGenre}&preferredCountry=us&genreName=${selectedGenre}k&preferredCountry=us&city=${city.value}&size=60`;
-      fetch(ticketMasterUrl)
-        .then((data) => {
-          //console.log(data);
-          return data.json();
-        })
-        .then((completedata) => {
-          console.log(completedata);
-          let txt = "";
-          //let br = document.createElement("br")
-          let dataEvents = completedata._embedded.events;
-          dataEvents.forEach((data) => {
-            if (data.hasOwnProperty("priceRanges")) {
-              txt += ` <div class="concert-card">
-          <h1 class="title">${data.name}</h1>
       let ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=1coN1aL4iKW9A3ex76AtrnJa4sBOaFua&locale=*&classificationName=${selectedGenre}&preferredCountry=us&genreName=${selectedGenre}k&preferredCountry=us&city=${city.value}&size=60`
       fetch(ticketMasterUrl).then((data) => {
         //console.log(data);
@@ -172,13 +116,6 @@ document.querySelector("#trending").style.display = "none";
           <p class="price">${"Price Ranges: " + "$" + data.priceRanges[0].min + " - " + "$" + data.priceRanges[0].max}</p>
           </div>
           </div>`;
-            }
-            //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ;
-            document.getElementById("results").innerHTML = txt;
-          });
-        })
-        .catch((err) => {
-          console.log(err);
           }
           //txt += data.name + " " + data.dates.start.dateTime + " " + data._embedded.venues[0].address.line1 + data.images[0].url + " " ; 
           document.getElementById("results").innerHTML = txt;
@@ -190,9 +127,10 @@ document.querySelector("#trending").style.display = "none";
 
     }
   });
-};
-window.addEventListener("load", loadPage);
 
 }
 window.addEventListener("load", loadPage)
+
+
+
 
